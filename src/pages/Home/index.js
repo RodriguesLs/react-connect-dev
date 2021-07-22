bd8cd88d9b96cc3d9748109f72baa214d8795449
Rodrigues <fr.costa95@gmail.com>
2021-07-21 23:13:52 -0300

    feat(setup-2): remove unused files and install material-ui
    	Removed unused files generated by create-react-app.
    	Installed material ui, using yarn.
    	Organize the structure of code (split header).

---
+++ src/pages/Home/index.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import './style.css';
+import Header from './components/Header.jsx';
+
+const Home = () => {
+  return (
+    <>
+      <Header />
+      <main className="main">
+        <div className="navbar">
+        </div>
+        <div className="feed">
+
+        </div>
+      </main>
+    </>
+  );
+}
+
+export default Home;
